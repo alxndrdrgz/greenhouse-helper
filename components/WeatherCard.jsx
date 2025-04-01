c76a9f6298effd5b2782efc16d467932fd6fb768
Alex Rodriguez-Sta. Ana <alexander.e.rsa@gmail.com>
2025-04-01 12:30:27 -0400

    Added country code and feels like to API call

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -4,10 +4,11 @@ export default function WeatherCard({ data }) {
     <div className="weather-card border rounded-sm">
       <h2 className="text-4xl font-extrabold bg-black text-white w-full px-8 py-2">Weather Information:</h2>
       <div className="p-4 text-center">        
-        <h2 className="text-2xl font-bold italic pt-2">
-          {data.city}
+        <h2 className="text-2xl font-bold italic p-2">
+          {`${data.city}, ${data.country} `} 
         </h2>
         <p><span className="font-bold">Temperature:</span> {data.temperature}</p>
+        <p><span className="font-bold">Feels Like:</span> {data.feelsLike}</p>
         <p><span className="font-bold">Humidity:</span> {data.humidity}%</p>
         <p><span className="font-bold">Weather:</span> {data.weather}</p>
         <p> <span className="font-bold">Windspeed:</span> {data.windSpeed}</p>
--- src/app/api/openweather/route.js
+++ src/app/api/openweather/route.js
@@ -6,16 +6,20 @@ export async function GET(req) {
   const fullUrl = new URL(req.url, baseURL); // Use base URL for parsing
 
   const city = fullUrl.searchParams.get("city") || "Toronto"; 
+  const country = fullUrl.searchParams.get("country") ||"CA";
+
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`
     );
 
     return new Response(
       JSON.stringify([{
         id: response.data.id,
         city: response.data.name,
+        country: response.data.sys.country,
         temperature: response.data.main.temp,
+        feelsLike: response.data.main.feels_like,
         humidity: response.data.main.humidity,
         weather: response.data.weather[0].description,
         windSpeed: response.data.wind.speed,
